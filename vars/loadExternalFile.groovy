2ba586f9710803a19b007b4cce6bc872b58c1cf3
ddhananjay <dhananjayd510@gmail.com>
2024-11-03 21:55:18 -0600

    Create loadExternalFile.groovy

---
+++ vars/loadExternalFile.groovy
@@ -0,0 +1,5 @@
+def call(Map : config =[:]) {
+  def scriptContent = libraryResource "scripts/${config.name}"
+  writeFile file : "${config.name}", text: scriptContent
+  sh "chmod a+x ./${config-name)"
+}
