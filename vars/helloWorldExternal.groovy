69d538b55a6c40cd72068259aeaa863b65a958a4
ddhananjay <dhananjayd510@gmail.com>
2024-11-03 22:19:07 -0600

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -2,5 +2,6 @@ def call(Map config) {
  sh "echo from external"
  def loadConfig = [name: 'hello-world.sh']
  loadExternalFile(loadConfig)
+ echo "fileWrote"
  sh "./hello-world.sh ${config.name} ${config.day}"
 }
