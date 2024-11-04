9598100d195b50ec58d1346fbdaca75354debfb7
ddhananjay <dhananjayd510@gmail.com>
2024-11-03 22:25:40 -0600

    Update loadExternalFile.groovy

--- vars/loadExternalFile.groovy
+++ vars/loadExternalFile.groovy
@@ -1,8 +1,5 @@
 def call(Map config) {
-  echo "${config}"
-  echo "${config.name}"
   def scriptContent = libraryResource "scripts/${config.name}"
-  echo "${scriptContent}"
   writeFile file: '${config.name}', text: scriptContent
   sh "chmod a+x ./'${config.name}'"
 }
